05ac553be4fe6332882496beaf35b0d8c8217a8a
Vishesh Agrawal <agrawalvishesh9271@gmail.com>
2023-08-01 11:01:30 +0530

    Day 4 Theme Complete

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import HomePage from './components/HomePage';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import BookList from './components/BooksList';
 import PageNotFound from './components/PageNotFound';
+import { Button, ThemeProvider, createTheme } from '@mui/material';
+import Form from './components/Form';
 
 function App() {
+  const theme = createTheme({
+    components: {
+      // Name of the component
+      MuiButton: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            backgroundColor: 'red',
+            '&:hover': {
+              background: "green",
+            },
+          },
+        },
+      },
+    },
+  });
+
   return (
     <div className="App">
-
-      <BrowserRouter>
-        <div>
-          <NavLink style={{ marginRight: 10 }} to="/">Home</NavLink>
-          <NavLink style={{ marginLeft: 10 }} to="/Book">Book</NavLink>
-        </div>
-        <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/Book' element={<BookList />}></Route>
-          <Route path='/*' element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink style={{ marginLeft: 10 }} to="/Book">Book</NavLink>
+            <NavLink style={{ marginLeft: 10 }} to="/Form">Form</NavLink>
+          </div>
+          <Routes>
+            <Route path='/' element={<HomePage />}></Route>
+            <Route path='/Book' element={<BookList />}></Route>
+            <Route path='/Form' element={<Form />}></Route>
+            <Route path='/*' element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/globalStyles.js
@@ -0,0 +1,5 @@
+const GlobalStyles = {
+    navbar: {
+        marginTop: '20px',
+    }
+}
