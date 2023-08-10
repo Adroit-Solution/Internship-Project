5e210a5114ef24c9cfbcff6980bd5231e1981b20
Vishesh Agrawal <agrawalvishesh9271@gmail.com>
2023-08-10 10:42:04 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './components/HomePage';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import BookList from './components/BooksList';
-import PageNotFound from './components/PageNotFound';
-import { Button, ThemeProvider, createTheme } from '@mui/material';
-import Form from './components/Form';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import MainNavigation from "./components/MainNavigation";
+import { AuthWrapper } from "./context/auth";
+import loader from "../src/assets/images/loader.gif";
+import { CartWrapper } from "./context/cart";
 
-function App() {
-  const theme = createTheme({
-    components: {
-      // Name of the component
-      MuiButton: {
-        styleOverrides: {
-          // Name of the slot
-          root: {
-            // Some CSS
-            backgroundColor: 'red',
-            '&:hover': {
-              background: "green",
-            },
-          },
-        },
-      },
-    },
-  });
-
+const App = () => {
   return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<></>}>
         <BrowserRouter>
-          <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink style={{ marginLeft: 10 }} to="/Book">Book</NavLink>
-            <NavLink style={{ marginLeft: 10 }} to="/Form">Form</NavLink>
-          </div>
-          <Routes>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='/Book' element={<BookList />}></Route>
-            <Route path='/Form' element={<Form />}></Route>
-            <Route path='/*' element={<PageNotFound />}></Route>
-          </Routes>
+          <AuthWrapper>
+            <CartWrapper>
+              <div className="loader-wrapper">
+                <img src={loader} alt="loader" />
+              </div>
+              <div className="wrapper">
+                <Header />
+                <main>
+                  <MainNavigation />
+                </main>
+                <Footer />
+              </div>
+              <ToastContainer />
+            </CartWrapper>
+          </AuthWrapper>
         </BrowserRouter>
-      </ThemeProvider>
-    </div>
+      </React.Suspense>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
