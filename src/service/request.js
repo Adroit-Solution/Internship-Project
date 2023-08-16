7d2a05588b86a1d06068d6de338b3272875b3e52
Vishesh Agrawal <agrawalvishesh9271@gmail.com>
2023-08-16 13:25:16 +0530

    Full Project

--- src/service/request.js
+++ src/service/request.js
@@ -2,12 +2,9 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const request = axios.create({
-  //  baseURL: "https://web1.anasource.com/BookStore/api/BookStore/", // url = base url + request url
-  //   baseURL: "http://localhost:5000/",
+
   baseURL: "https://book-e-sell-node-api.vercel.app/",
-  // baseURL: "https://helperland1.azurewebsites.net/",
-  // baseURL: "https://helperland1.azurewebsites.net/",
-  //  baseURL: "http://192.168.1.20/",
+
   timeout: 12400000,
   responseType: "json",
 });
